5defdb4108717fda9ceafa212678ec1d9b1eb3ba
Alex Werner <obusco@gmail.com>
2024-01-19 15:32:59 +0100

    feat(Askr): generate or import keyPair + ask method

--- src/primitives/Askr/Askr.js
+++ src/primitives/Askr/Askr.js
@@ -9,6 +9,8 @@ import getPath from "./methods/emit.js";
 import handshake from "./methods/handshake.js";
 import on from "./methods/on.js";
 import start from "./methods/start.js";
+import generateRandomPair from "../../utils/generateRandomPair.js";
+import ConnectedState from "../ConnectedState/ConnectedState.js";
 
 class Askr {
     constructor(props = {}) {
@@ -24,18 +26,61 @@ class Askr {
         });
         this.peerList = new PeerList();
         this.listeners = {};
+
+        if(props.keys) {
+            if(props.keys === true){
+                // We are asked to generate keypairs, but needs to be async because of browser compatibility w/ packaging currently
+                // FIXME.
+                generateRandomPair(512)
+                    .then((keypair) => {
+                    this.keys = {
+                        publicKey: keypair.publicKey,
+                        privateKey: keypair.privateKey
+                    }
+                });
+            } else{
+                // We assume the keys are provided as an object
+                this.keys = props.keys;
+            }
+        }
+        if(props.useConnectedState){
+            this.connectedState = new ConnectedState(props.connectedState);
+        }
+
     }
 
     _handleHandshake(data, senderPeer) {
         this.logger.method('_handleHandshake').log(`Received handshake from ${senderPeer.getID()}`);
         const {payload} = data;
         // Update the peer list with the received network map
-        this.peerList.updateFromNetworkMap(payload);
+        // this.peerList.updateFromNetworkMap(payload);
 
         // senderPeer.send({
         //   type: 'handshake',
         //   payload: this.peerList.getNetworkMap()
         // });
+    }
+
+    async ask( props = {}) {
+        return new Promise(async (resolve, reject) => {
+            const peer = this.addPeer(props.host, props.port);
+            await peer.connect();
+
+            const message = {
+               workspace: props.workspace,
+               mid: props?.messageId ?? 1,
+                command: props.command,
+                data: props.data ?? {}
+            }
+
+            message.data.signature = true;
+
+            peer.send(message);
+            peer.connection.on('data', (data) => {
+                const response = JSON.parse(data.toString());
+                resolve(response);
+            });
+        });
     }
 }
 
